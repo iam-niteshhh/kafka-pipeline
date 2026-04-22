9e2d6046279df561fb27c8473b33ed1171f851a9
Nitesh Saini <56442673+iam-niteshhh@users.noreply.github.com>
2026-04-22 21:04:05 +0000

    added database concepts

--- consumer.py
+++ consumer.py
@@ -1,5 +1,6 @@
 from kafka import KafkaConsumer
 import json
+import psycopg2
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -11,7 +12,25 @@ consumer = KafkaConsumer(
     enable_auto_commit=True,
 )
 
-logging.info("Starting to consume events...")
+# Postgres Conncetion Setup
+conn = psycopg2.connect(
+    host="localhost",
+    database="events_db",
+    user="kafka",
+    password="kafka",
+    port=5432
+)
+
+cursor = conn.cursor()
+
+logging.info("Consumer + DB Started...")
+
 for message in consumer:
     event = message.value
     logging.info(f"Consumed event: {event}")
+
+    cursor.execute(
+        "INSERT INTO user_events (user_id, event, device, timestamp) VALUES (%s, %s, %s, %s)",
+        (event['user_id'], event['event'], event['device'], event['timestamp'])
+    )
+    conn.commit()
