2da2ae61cab5ba8e21b170a01d2969dcaca8c981
Nitesh Saini <56442673+iam-niteshhh@users.noreply.github.com>
2026-04-24 18:41:26 +0000

    added retry and DLQ logics

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# kafka-pipeline
+# Real-Time Kafka Streaming Pipeline
+
+## Overview
+This project demonstrates a real-time data streaming pipeline using Kafka, Python, and PostgreSQL. It simulates user event ingestion, processes data in real-time, and stores it in a relational database for analytics.
+
+---
+
+## Architecture
+Producer → Kafka → Consumer Group → Batch Processor → PostgreSQL
+
+
+---
+
+## Tech Stack
+- Python  
+- Apache Kafka  
+- PostgreSQL  
+- Docker  
+- Kafka-Python  
+
+---
+
+## Features
+- Real-time event streaming using Kafka  
+- Consumer group-based parallel processing  
+- Batch inserts for optimized database performance  
+- Scalable partition-based architecture  
+- Dockerized setup for easy deployment  
+
+---
+
+## Data Flow
+1. Python producer generates user events  
+2. Kafka ingests and buffers data  
+3. Consumer group processes events in parallel  
+4. Batch processor writes to PostgreSQL  
+
+---
+
+## Key Concepts Used
+- Kafka partitions  
+- Consumer groups  
+- At-least-once delivery  
+- Batch processing  
+- Streaming architecture  
+
+---
+
+## How to Run
+```bash
+docker-compose up -d
+python producer.py
+python consumer.py
+
+---
+## Database Schema
+
+CREATE TABLE user_events (
+    user_id INT,
+    event TEXT,
+    device TEXT,
+    timestamp DOUBLE PRECISION
+);
--- consumer.py
+++ consumer.py
@@ -1,8 +1,14 @@
 from kafka import KafkaConsumer
+from kafka import KafkaProducer
 import json
 import psycopg2
 import logging
+import random
+import time
+
 logging.basicConfig(level=logging.INFO)
+BATCH_SIZE  = 20
+MAX_RETRIES = 4
 
 consumer = KafkaConsumer(
     'user_events',
@@ -10,6 +16,12 @@ consumer = KafkaConsumer(
     auto_offset_reset='latest',
     value_deserializer=lambda x: json.loads(x.decode('utf-8')),
     enable_auto_commit=True,
+    group_id='event_consumers'
+)
+
+dlq_producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda x: json.dumps(x).encode('utf-8')
 )
 
 # Postgres Conncetion Setup
@@ -25,12 +37,45 @@ cursor = conn.cursor()
 
 logging.info("Consumer + DB Started...")
 
+batch = []
 for message in consumer:
+    # time.sleep(5) # Simulate processing time
+    retries = 0
     event = message.value
-    logging.info(f"Consumed event: {event}")
+    
+    # logging.info(f"Consumed event: {event}")
+    batch.append((
+        event['user_id'],
+        event['event'],
+        event['device'],
+        event['timestamp']
+    ))
+    logging.info(f"Batch size: {len(batch)}")
 
-    cursor.execute(
-        "INSERT INTO user_events (user_id, event, device, timestamp) VALUES (%s, %s, %s, %s)",
-        (event['user_id'], event['event'], event['device'], event['timestamp'])
-    )
-    conn.commit()
+    if len(batch) >= BATCH_SIZE:
+        while retries < MAX_RETRIES:
+
+            logging.info(f"Processing batch of {len(batch)} events...")
+            try:
+                if random.random() < 0.2:  # Simulate random failure
+                    logging.warning("Simulated DB failure. Retrying...")
+                    raise Exception("Simulated DB failure")
+                cursor.executemany(
+                    "INSERT INTO user_events (user_id, event, device, timestamp) VALUES (%s, %s, %s, %s)",
+                    batch
+                )
+                conn.commit()
+                batch.clear()
+                break
+            except Exception as e:
+                logging.error(f"Error inserting batch: {e}", exc_info=True)
+                conn.rollback()
+                retries += 1
+                logging.info(f"Retrying batch... Attempt {retries}/{MAX_RETRIES}")
+
+        if retries == MAX_RETRIES:
+            logging.error("Max retries reached. Skipping batch.")
+            dlq_producer.send('user_events_dlq', value=batch)
+            batch.clear()  # Clear batch after sending to DLQ
+            break
+            
